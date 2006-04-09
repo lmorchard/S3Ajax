81e01adbc76816247110a0feb59ab78c53975262
deusx <devnull@localhost>
2006-04-09 06:09:50 +0000

    [svn r752] [wiki:S3Ajax]: Added optional Dojo storage for auth credentials, removed MochiKit dependencies, added XML-to-Object code for interpreting listings and ACLs, totally revamped S3Ajax.httpClient API calls

--- js/S3Ajax.js
+++ js/S3Ajax.js
@@ -6,18 +6,28 @@
     Share and Enjoy.
 
     Requires:
-        http://www.mochikit.com/
         http://pajhome.org.uk/crypt/md5/sha1.js
         http://www.svendtofte.com/code/date_format/formatDate.js
 */
 
+// TODO: Figure out if Safari doesn't support PUT and Delete
+
 S3Ajax = {
+
+    // Flip this to true to get lots of wonky logging.
+    DEBUG: false,
+
+    // Defeat caching with query params on GET requests?
+    DEFEAT_CACHE: false,
+
+    // Skip using Dojo or MochiKit XMLHttpRequest facilities
+    USE_CUSTOM_XHR: true,
     
     /**
         DANGER WILL ROBINSON - Do NOT fill in your KEY_ID and SECRET_KEY 
         here.  These should be supplied by client-side code, and not 
         stored in any server-side files.  Failure to protect your S3
-        credentials will result in undesired people doing nasty things
+        credentials will result in surly people doing nasty things
         on your tab.
 
         For example, scoop values up from un-submitted form fields like so:
@@ -31,67 +41,182 @@ S3Ajax = {
 
     /**
         Perform an authenticated S3 HTTP query.
-
-        TODO: Remove dependancy on MochiKit?
     */
-    httpClient: function(method, resource, params, content, content_type, acl, meta) {
+    httpClient: function(kwArgs) {
+        var _this = this;
+        
+        // If need to defeat cache, toss in a date param on GET.
+        if (this.DEFEAT_CACHE && kwArgs.method == "GET") {
+            if (!kwArgs.params) kwArgs.params = {};
+            kwArgs.params["___"] = new Date().getTime();
+        }
 
         // Prepare the query string and URL for this request.
-        var qs   = (params) ? '?'+queryString(params) : '';
-        var url  = this.URL + resource + qs;
+        var qs   = (kwArgs.params) ? '?'+this.queryString(kwArgs.params) : '';
+        var url  = this.URL + kwArgs.resource + qs;
         var hdrs = {};
 
         // Handle Content-Type header
-        if (!content_type) content_type = 'text/plain';
-        hdrs['Content-Type'] = content_type;
+        if (!kwArgs.content_type && kwArgs.method == 'PUT') 
+            kwArgs.content_type = 'text/plain';
+        if (kwArgs.content_type)
+            hdrs['Content-Type'] = kwArgs.content_type;
+        else
+            kwArgs.content_type = '';
 
         // Set the timestamp for this request.
         var http_date = this.httpDate();
         hdrs['Date']  = http_date;
 
-        // Huh.  The Perl s3curl script defines a $contentMD5, but it never uses it.
         var content_MD5 = '';
-        // if (content) content_MD5 = string.hashMD5(content)
+        /*
+        // TODO: Fix this Content-MD5 stuff.
+        if (kwArgs.content && kwArgs.content.hashMD5) {
+            content_MD5 = kwArgs.content.hashMD5();
+            hdrs['Content-MD5'] = content_MD5;
+        }
+        */
 
         // Handle the ACL parameter
         var acl_header_to_sign = '';
-        if (acl) {
-            hdrs['x-amz-acl'] = acl;
-            acl_header_to_sign = "x-amz-acl:"+acl+"\n";
+        if (kwArgs.acl) {
+            hdrs['x-amz-acl'] = kwArgs.acl;
+            acl_header_to_sign = "x-amz-acl:"+kwArgs.acl+"\n";
         }
         
         // Handle the metadata headers
         var meta_to_sign = '';
-        if (meta) {
-            for (var k in meta) {
-                hdrs['x-amz-meta-'+k] = meta[k];
-                meta_to_sign += "x-amz-meta-"+k+":"+meta[k]+"\n";
+        if (kwArgs.meta) {
+            for (var k in kwArgs.meta) {
+                hdrs['x-amz-meta-'+k] = kwArgs.meta[k];
+                meta_to_sign += "x-amz-meta-"+k+":"+kwArgs.meta[k]+"\n";
             }
         }
 
         // Build the string to sign for authentication.
-        var s = method + "\n";
+        var s = kwArgs.method + "\n";
         s = s + content_MD5 + "\n";
-        s = s + content_type + "\n";
+        s = s + kwArgs.content_type + "\n";
         s = s + http_date + "\n";
         s = s + acl_header_to_sign;
         s = s + meta_to_sign;
-        s = s + resource;
+        s = s + kwArgs.resource;
 
         // Sign the string with our SECRET_KEY.
-        var signature = b64_hmac_sha1(this.SECRET_KEY, s)+'=';
+        var signature = this.hmacSHA1(s, this.SECRET_KEY);
         hdrs['Authorization'] = "AWS "+this.KEY_ID+":"+signature;
 
-        // Prepare and fire off the request, return the Deferred.
-        var req = getXMLHttpRequest();
-        req.open(method, url, true);
+        // Perform the HTTP request.
+        var req = this.getXMLHttpRequest();
+        req.open(kwArgs.method, url, true);
         for (var k in hdrs) req.setRequestHeader(k, hdrs[k]);
-        var d = sendXMLHttpRequest(req, content);
-        return d;
+        req.onreadystatechange = function() {
+            if (req.readyState == 4) {
+
+                // Parse the XML if needed.
+                var obj = null;
+                if (req.responseXML && kwArgs.parseXML != false)
+                    obj = _this.xmlToObj(req.responseXML);
+
+                // Stash away the last request details, if DEBUG active.
+                if (_this.DEBUG) {
+                    window._lastreq = req;
+                    window._lastobj = obj;
+                }
+                
+                // Dispatch to appropriate handler callback
+                if (req.status >= 400 && kwArgs.error)
+                    return kwArgs.error(req, obj);
+                else
+                    return kwArgs.load(req, obj);
+            }
+        }
+        req.send(kwArgs.content);
+        return req;
+    },
+
+    /**
+        Turn a simple structure of nested XML elements into a 
+        JavaScript object.
+
+        TODO: Handle attributes?
+    */
+    xmlToObj: function(parent) {
+        var obj = {};
+        var cdata = '';
+        var is_struct = false;
+
+        for(var i=0,node; node=parent.childNodes[i]; i++) {
+            if (3 == node.nodeType) { 
+                cdata += node.nodeValue;
+            } else {
+                is_struct = true;
+                var name  = node.nodeName;
+                var val   = arguments.callee(node);
+
+                if (!obj[name]) {
+                    // No such key yet, so start with a single value.
+                    obj[name] = val;
+                } else if (obj[name].length) {
+                    // This is a list of values to append this one to the end.
+                    obj[name].push(val);
+                } else {
+                    // Has been a single value up till now, so convert to list.
+                    obj[name] = [ obj[name], val ];
+                }
+            }
+        }
+
+        // If any subnodes were found, return a struct - else return cdata.
+        return (is_struct) ? obj : cdata;
+    },
+
+    /**
+        Wrap object-to-query-string encoding.
+    */
+    queryString: function(params) {
+        var l = [];
+        for (k in params) l.push(k+'='+encodeURIComponent(params[k]))
+        return l.join("&");
+    },
+
+    /**
+        Shamelessly swiped from MochiKit/Async.js
+    */
+    getXMLHttpRequest: function() {
+        var self = arguments.callee;
+        if (!self.XMLHttpRequest) {
+            var tryThese = [
+                function () { return new XMLHttpRequest(); },
+                function () { return new ActiveXObject('Msxml2.XMLHTTP'); },
+                function () { return new ActiveXObject('Microsoft.XMLHTTP'); },
+                function () { return new ActiveXObject('Msxml2.XMLHTTP.4.0'); },
+                function () { return null; }
+            ];
+            for (var i = 0; i < tryThese.length; i++) {
+                var func = tryThese[i];
+                try {
+                    self.XMLHttpRequest = func;
+                    return func();
+                } catch (e) {
+                    // pass
+                }
+            }
+        }
+        return self.XMLHttpRequest();
+    },
+
+    /**
+        Abstract HMAC SHA1 signature calculation.
+    */
+    hmacSHA1: function(data, secret) {
+        // TODO: Alternate Dojo implementation
+        return b64_hmac_sha1(secret, data)+'=';
     },
     
     /**
         Return a date formatted appropriately for HTTP Date header.
+
         TODO: Rewrite to remove formatDate.js dependancy.
         See: http://www.svendtofte.com/code/date_format/
     */
--- js/play.js
+++ js/play.js
@@ -2,12 +2,43 @@
     Play around with S3Ajax!
 */
 
+if (window.dojo) {
+    // dojo.setModulePrefix("dojo", "js/dojo");
+    // dojo.require("dojo.io.*");
+    // dojo.require("dojo.crypto.SHA1");
+    dojo.require("dojo.storage.*");
+}
+
 Play = {
 
     /**
     */
     init: function() {
-        log("STARTING UP!");
+        var _this = this;
+
+        S3Ajax.DEBUG = true;
+
+        // HACK: Wait for the storage flash to become available, 
+        if (window.dojo) {
+            this._storage_wait = setInterval(function() {
+                if ($('dojo-storeContainer')) {
+                    clearInterval(_this._storage_wait);
+                    _this._storage_wait = null;
+                    _this.recall();
+                }
+            }, 100);
+        }
+    },
+    
+    /**
+    */
+    recall: function() {
+        if (window.dojo && dojo.storage.get('key_id', '/S3Ajax')) {
+            S3Ajax.KEY_ID     = dojo.storage.get('key_id',     '/S3Ajax');
+            S3Ajax.SECRET_KEY = dojo.storage.get('secret_key', '/S3Ajax');
+            $('key_id').value = S3Ajax.KEY_ID;
+            $('update_msg').innerHTML = 'Login fetched from local storage: '+(new Date());
+        }
     },
 
     /**
@@ -17,9 +48,13 @@ Play = {
         S3Ajax.KEY_ID     = $('key_id').value;
         S3Ajax.SECRET_KEY = $('secret_key').value;
 
+        if (window.dojo) {
+            dojo.storage.set('key_id',     S3Ajax.KEY_ID,     '/S3Ajax');
+            dojo.storage.set('secret_key', S3Ajax.SECRET_KEY, '/S3Ajax');
+        }
+
         // Indicate success.
         $('update_msg').innerHTML = 'Last updated: '+(new Date());
-        log("Logged in "+S3Ajax.KEY_ID);
     },
 
     /**
@@ -28,19 +63,21 @@ Play = {
     download: function() {
         var _this = this;
         var resource = $('resource').value;
-        var d = S3Ajax.httpClient("GET", resource);
-        
-        d.addCallback(function(rv) {
-            $('content').value = rv.responseText;
-            $('xfer_msg').innerHTML = "Download succeeded at "+(new Date());
-            _this.lastRequest = rv;
+
+        var req = S3Ajax.httpClient({
+            method:   'GET',
+            resource: resource,
+            load: function(rv) {
+                $('content').value = rv.responseText;
+                $('xfer_msg').innerHTML = "Download succeeded at "+(new Date());
+                _this.lastRequest = rv;
+            },
+            error: function(rv) {
+                $('xfer_msg').innerHTML = "Download failed at "+(new Date());
+                _this.lastRequest = rv;
+            }
         });
 
-        d.addErrback(function(rv) {
-            $('xfer_msg').innerHTML = "Download failed at "+(new Date());
-            log(rv);
-            _this.lastRequest = rv;
-        });
     },
 
     /**
@@ -50,23 +87,98 @@ Play = {
         var _this = this;
         var resource = $('resource').value;
         var content  = $('content').value;
-        var d = S3Ajax.httpClient("PUT", resource, null, content, 
-            "text/plain", "public-read", {'posted-by':'S3Ajax'});
-        
-        d.addCallback(function(rv) {
-            $('xfer_msg').innerHTML = "Upload succeeded at "+(new Date());
-            _this.lastRequest = rv;
+
+        var req = S3Ajax.httpClient({
+            method:       'PUT',
+            resource:     resource,
+            content_type: "text/plain",
+            content:      content,
+            meta:         {'posted-by':'S3Ajax'},
+            acl:          "public-read",
+            load:  function(rv) {
+                $('xfer_msg').innerHTML = "Upload succeeded at "+(new Date());
+                _this.lastRequest = rv;
+            },
+            error: function(rv) {
+                $('xfer_msg').innerHTML = "Upload failed at "+(new Date());
+                _this.lastRequest = rv;
+            }
+        });
+    },
+
+
+    /**
+
+    */
+    list: function() {
+        var _this = this;
+        var resource = $('resource').value;
+
+        var req = S3Ajax.httpClient({
+            method:   'GET',
+            resource: resource,
+            /* params: {'prefix':'js', 'max-keys':10 }, */
+            load: function(req, obj) {
+                if (obj.ListBucketResult) {
+                    var out = '';
+                    var contents = obj.ListBucketResult.Contents;
+                    for (var i=0, item; item=contents[i]; i++) {
+                        out += item.LastModified + ' ' + item.Key + ' (' + item.Size + ')\n';
+                    }
+                    $('content').value = out;
+                }
+            },
+            error: function(req) {
+                $('xfer_msg').innerHTML = "Bucket list failed at "+(new Date());
+            }
         });
 
-        d.addErrback(function(rv) {
-            $('xfer_msg').innerHTML = "Upload failed at "+(new Date());
-            log(rv);
-            _this.lastRequest = rv;
+    },
+
+    /**
+
+    */
+    listbuckets: function() {
+        var _this = this;
+        var req = S3Ajax.httpClient({
+            method:   'GET',
+            resource: '/',
+            load: function(req, obj) {
+                if (obj.ListAllMyBucketsResult) {
+                    var out = '';
+                    var buckets = obj.ListAllMyBucketsResult.Buckets.Bucket;
+                    for (var i=0, bucket; bucket=buckets[i]; i++) {
+                        out += bucket.Name + ' ['+bucket.CreationDate+']\n';
+                    }
+                    $('content').value = out;
+                }
+            },
+            error: function(req) {
+                $('xfer_msg').innerHTML = "Buckets list failed at "+(new Date());
+            }
         });
+
     },
 
     /* Help protect against errant end-commas */
     EOF: null
+}
+
+if (!window['addLoadEvent']) {
+    function addLoadEvent(func) {
+        var oldonload = window.onload;
+        if (typeof window.onload != 'function') {
+            window.onload = func;
+        } else {
+            window.onload = function() {
+                oldonload();
+                func();
+            }
+        }
+    }
+}
+if (!window['$']) {
+    function $(id) { return document.getElementById(id); }
 }
 
 addLoadEvent(function(){ Play.init() });
@@ -101,7 +213,8 @@ logFire = function() {
         printfire( arguments[0], arguments[1]); 
     } 
 }
-logger.addListener('firebug', null, function (msg) { 
-    printfire(msg.level + sum(map(function (i) { return ', ' + i; }, msg.info))); 
-});
-
+if (window['logger'] && logger.addListener) {
+    logger.addListener('firebug', null, function (msg) { 
+        printfire(msg.level + sum(map(function (i) { return ', ' + i; }, msg.info))); 
+    });
+}
