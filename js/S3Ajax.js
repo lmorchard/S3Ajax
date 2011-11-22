15bd08b944e05490f661101ce26cdb8b079e987c
Les Orchard <l.m.orchard@pobox.com>
2011-11-21 21:35:00 -0500

    Refreshing my brain on old code by way of mangling it through JSHint

--- js/S3Ajax.js
+++ js/S3Ajax.js
@@ -1,97 +1,86 @@
-/**
-    S3Ajax v0.1 - An AJAX wrapper package for Amazon S3
+//  S3Ajax v0.1 - An AJAX wrapper package for Amazon S3
+//
+//  http://decafbad.com/trac/wiki/S3Ajax
+//  l.m.orchard@pobox.com
+//  Share and Enjoy.
+//
+//  Requires:
+//      http://pajhome.org.uk/crypt/md5/sha1.js
 
-    http://decafbad.com/trac/wiki/S3Ajax
-    l.m.orchard@pobox.com
-    Share and Enjoy.
+// Constructor
+function S3Ajax () {
+    return this.init.apply(this, arguments);
+}
 
-    Requires:
-        http://pajhome.org.uk/crypt/md5/sha1.js
-*/
+// Methods and properties
+S3Ajax.prototype = {
 
-// TODO: Figure out if Safari doesn't support PUT and DELETE
-
-S3Ajax = {
-
-    // Defeat caching with query params on GET requests?
-    DEFEAT_CACHE: false,
-
-    // Default ACL to use when uploading keys.
-    DEFAULT_ACL: 'public-read',
-    
-    // Default content-type to use in uploading keys.
-    DEFAULT_CONTENT_TYPE: 'text/plain',
-
-    /**
-        DANGER WILL ROBINSON - Do NOT fill in your KEY_ID and SECRET_KEY 
-        here.  These should be supplied by client-side code, and not 
-        stored in any server-side files.  Failure to protect your S3
-        credentials will result in surly people doing nasty things
-        on your tab.
-
-        For example, scoop values up from un-submitted form fields like so:
-
-        S3Ajax.KEY_ID     = $('key_id').value;
-        S3Ajax.SECRET_KEY = $('secret_key').value;
-    */
-    URL:        'http://s3.amazonaws.com',
-    KEY_ID:     '',
-    SECRET_KEY: '',
-
-    // Flip this to true to potentially get lots of wonky logging.
-    DEBUG: false,
-
-    /**
-        Get contents of a key in a bucket.
-    */
-    get: function(bucket, key, cb, err_cb) {
-        return this.httpClient({
-            method:   'GET',
-            resource: '/' + bucket + '/' + key,
-            load: function(req, obj) {
-                if (cb)     return cb(req, req.responseText);
-            },
-            error: function(req, obj) {
-                if (err_cb) return err_cb(req, obj);
-                if (cb)     return cb(req, req.responseText);
-            }
-        })
+    // Defaults for options accepted in constructor.
+    defaults: {
+        // Base URL for S3 API
+        base_url: 'http://s3.amazonaws.com',
+        // Key ID for credentials
+        key_id: null,
+        // Secret key for credentials
+        secret_key: null,
+        // Flip this to true to potentially get lots of wonky logging.
+        debug: false,
+        // Defeat caching with query params on GET requests?
+        defeat_cache: false,
+        // Default ACL to use when uploading keys.
+        default_acl: 'public-read',
+        // Default content-type to use in uploading keys.
+        default_content_type: 'text/plain; charset=UTF-8'
     },
 
-    /**
-        Head the meta of a key in a bucket.
-    */
-    head: function(bucket, key, cb, err_cb) {
-        return this.httpClient({
-            method:   'HEAD',
-            resource: '/' + bucket + '/' + key,
-            load: function(req, obj) {
-                if (cb)     return cb(req, req.responseText);
-            },
-            error: function(req, obj) {
-                if (err_cb) return err_cb(req, obj);
-                if (cb)     return cb(req, req.responseText);
+    // Initialize object (called from constructor)
+    init: function (options) {
+        this.options = options;
+        for (var k in this.defaults) {
+            if (this.defaults.hasOwnProperty(k)) {
+                this[k] = (typeof(options[k]) !== 'undefined') ?
+                    options[k] : this.defaults[k];
             }
-        })
+        }
+        return this;
     },
 
-    /**
-        Put data into a key in a bucket.
-    */
-    put: function(bucket, key, content/*, [params], cb, [err_cb]*/) {
+    // Get contents of a key in a bucket.
+    get: function (bucket, key, cb, err_cb) {
+        return this.httpClient({
+            method: 'GET',
+            resource: '/' + bucket + '/' + key,
+            load: cb, error: err_cb 
+        });
+    },
+
+    // Head the meta of a key in a bucket.
+    head: function (bucket, key, cb, err_cb) {
+        return this.httpClient({
+            method: 'HEAD',
+            resource: '/' + bucket + '/' + key,
+            load: cb, error: err_cb 
+        });
+    },
+
+    // Put data into a key in a bucket.
+    put: function (bucket, key, content/*, [params], cb, [err_cb]*/) {
 
         // Process variable arguments for optional params.
         var idx = 3;
         var params = {};
-        if (typeof arguments[idx] == 'object')
+        if (typeof arguments[idx] === 'object') {
             params = arguments[idx++];
+        }
         var cb     = arguments[idx++];
         var err_cb = arguments[idx++];
 
-        if (!params.content_type) 
-            params.content_type = this.DEFAULT_CONTENT_TYPE;
-        if (!params.acl)
-            params.acl = this.DEFAULT_ACL;
+        if (!params.content_type) {
+            params.content_type = this.default_content_type;
+        }
+        if (!params.acl) {
+            params.acl = this.default_acl;
+        }
 
         return this.httpClient({
             method:       'PUT',
@@ -100,108 +89,95 @@ S3Ajax = {
             content_type: params.content_type,
             meta:         params.meta,
             acl:          params.acl,
-            load: function(req, obj) {
-                if (cb)     return cb(req);
-            },
-            error: function(req, obj) {
-                if (err_cb) return err_cb(req, obj);
-                if (cb)     return cb(req, obj);
-            }
+            load: cb, error: err_cb 
         });
     },
 
-    /**
-        List buckets belonging to the account.
-    */
-    listBuckets: function(cb, err_cb) {
+    // List buckets belonging to the account.
+    listBuckets: function (cb, err_cb) {
         return this.httpClient({ 
-            method:'GET', resource:'/', 
+            method: 'GET', resource:'/', 
             force_lists: [ 'ListAllMyBucketsResult.Buckets.Bucket' ],
-            load: cb, error:err_cb 
+            load: cb, error: err_cb 
         });
     },
 
-    /**
-        Create a new bucket for this account.
-    */
-    createBucket: function(bucket, cb, err_cb) {
+    // Create a new bucket for this account.
+    createBucket: function (bucket, cb, err_cb) {
         return this.httpClient({ 
-            method:'PUT', resource:'/'+bucket, load:cb, error:err_cb 
+            method: 'PUT', resource: '/'+bucket, 
+            load: cb, error: err_cb 
         });
     },
 
-    /**
-        Delete an empty bucket.
-    */
-    deleteBucket: function(bucket, cb, err_cb) {
+    // Delete an empty bucket.
+    deleteBucket: function (bucket, cb, err_cb) {
         return this.httpClient({ 
-            method:'DELETE', resource:'/'+bucket, load:cb, error:err_cb 
+            method: 'DELETE', resource: '/'+bucket, 
+            load: cb, error: err_cb 
         });
     },
 
-    /**
-        Given a bucket name and parameters, list keys in the bucket.
-    */
-    listKeys: function(bucket, params, cb, err_cb) {
+    // Given a bucket name and parameters, list keys in the bucket.
+    listKeys: function (bucket, params, cb, err_cb) {
         return this.httpClient({
-            method:'GET', resource: '/'+bucket, 
+            method: 'GET', resource: '/'+bucket, 
             force_lists: [ 'ListBucketResult.Contents' ],
-            params:params, load:cb, error:err_cb
+            params: params, 
+            load: cb, error: err_cb
         });
     },
 
-    /**
-        Delete a single key in a bucket.
-    */
-    deleteKey: function(bucket, key, cb, err_cb) {
+    // Delete a single key in a bucket.
+    deleteKey: function (bucket, key, cb, err_cb) {
         return this.httpClient({
-            method:'DELETE', resource: '/'+bucket+'/'+key, load:cb, error:err_cb
+            method:'DELETE', resource: '/'+bucket+'/'+key,
+            load: cb, error: err_cb
         });
     },
 
-    /**
-        Delete a list of keys in a bucket, with optional callbacks
-        for each deleted key and when list deletion is complete.
-    */
-    deleteKeys: function(bucket, list, one_cb, all_cb) {
+    // Delete a list of keys in a bucket, with optional callbacks for each
+    // deleted key and when list deletion is complete.
+    deleteKeys: function (bucket, list, one_cb, all_cb) {
         var _this = this;
         
         // If the list is empty, then fire off the callback.
-        if (!list.length && all_cb) return all_cb();
+        if (!list.length && all_cb) { return all_cb(); }
 
         // Fire off key deletion with a callback to delete the 
         // next part of list.
         var key = list.shift();
-        this.deleteKey(bucket, key, function() {
-            if (one_cb) one_cb(key);
+        this.deleteKey(bucket, key, function () {
+            if (one_cb) { one_cb(key); }
             _this.deleteKeys(bucket, list, one_cb, all_cb);
         });
     },
 
-    /**
-        Perform an authenticated S3 HTTP query.
-    */
-    httpClient: function(kwArgs) {
+    // Perform an authenticated S3 HTTP query.
+    httpClient: function (kwArgs) {
         var _this = this;
         
         // If need to defeat cache, toss in a date param on GET.
-        if (this.DEFEAT_CACHE && ( kwArgs.method == "GET" || kwArgs.method == "HEAD" ) ) {
-            if (!kwArgs.params) kwArgs.params = {};
-            kwArgs.params["___"] = new Date().getTime();
+        if (this.defeat_cache && (kwArgs.method === "GET" || 
+                                  kwArgs.method === "HEAD") ) {
+            if (!kwArgs.params) { kwArgs.params = {}; }
+            kwArgs.params.___ = new Date().getTime();
         }
 
         // Prepare the query string and URL for this request.
-        var qs   = (kwArgs.params) ? '?'+queryString(kwArgs.params) : '';
-        var url  = this.URL + kwArgs.resource + qs;
+        var qs   = (kwArgs.params) ? '?'+this.queryString(kwArgs.params) : '';
+        var url  = this.base_url + kwArgs.resource + qs;
         var hdrs = {};
 
         // Handle Content-Type header
-        if (!kwArgs.content_type && kwArgs.method == 'PUT') 
+        if (!kwArgs.content_type && kwArgs.method === 'PUT') {
             kwArgs.content_type = 'text/plain';
-        if (kwArgs.content_type)
+        }
+        if (kwArgs.content_type) {
             hdrs['Content-Type'] = kwArgs.content_type;
-        else
+        } else {
             kwArgs.content_type = '';
+        }
 
         // Set the timestamp for this request.
         var http_date = this.httpDate();
@@ -227,73 +203,79 @@ S3Ajax = {
         var meta_to_sign = '';
         if (kwArgs.meta) {
             for (var k in kwArgs.meta) {
-                hdrs['x-amz-meta-'+k] = kwArgs.meta[k];
-                meta_to_sign += "x-amz-meta-"+k+":"+kwArgs.meta[k]+"\n";
+                if (kwArgs.meta.hasOwnProperty(k)) {
+                    hdrs['x-amz-meta-'+k] = kwArgs.meta[k];
+                    meta_to_sign += "x-amz-meta-"+k+":"+kwArgs.meta[k]+"\n";
+                }
             }
         }
 
         // Only perform authentication if non-anonymous and credentials available
-        if (kwArgs['anonymous'] != true && this.KEY_ID && this.SECRET_KEY) {
+        if (kwArgs.anonymous !== true && this.key_id && this.secret_key) {
 
             // Build the string to sign for authentication.
-            var s; 
-            s  = kwArgs.method + "\n";
-            s += content_MD5 + "\n";
-            s += kwArgs.content_type + "\n";
-            s += "\n"; // was Date header, don't work nomore with modern browsers.
-            s += 'x-amz-date:' + http_date + "\n";
-            s += acl_header_to_sign;
-            s += meta_to_sign;
-            s += kwArgs.resource;
+            var s = [
+                kwArgs.method, "\n",
+                content_MD5, "\n",
+                kwArgs.content_type, "\n",
+                "\n", // was Date header, no longer works with modern browsers.
+                acl_header_to_sign,
+                'x-amz-date:', http_date, "\n",
+                meta_to_sign,
+                kwArgs.resource
+            ].join('');
 
-            // Sign the string with our SECRET_KEY.
-            var signature = this.hmacSHA1(s, this.SECRET_KEY);
-            hdrs['Authorization'] = "AWS "+this.KEY_ID+":"+signature;
+            // Sign the string with our secret_key.
+            var signature = this.hmacSHA1(s, this.secret_key);
+            hdrs.Authorization = "AWS "+this.key_id+":"+signature;
         }
 
         // Perform the HTTP request.
-        var req = getXMLHttpRequest();
+        var req = this.getXMLHttpRequest();
         req.open(kwArgs.method, url, true);
-        for (var k in hdrs) req.setRequestHeader(k, hdrs[k]);
-        req.onreadystatechange = function() {
-            if (req.readyState == 4) {
+        for (var j in hdrs) { 
+            if (hdrs.hasOwnProperty(j)) {
+                req.setRequestHeader(j, hdrs[j]);
+            }
+        }
+        req.onreadystatechange = function () {
+            if (req.readyState === 4) {
 
                 // Pre-digest the XML if needed.
                 var obj = null;
-                if (req.responseXML && kwArgs.parseXML != false)
+                if (req.responseXML && kwArgs.parseXML !== false) {
                     obj = _this.xmlToObj(req.responseXML, kwArgs.force_lists);
+                }
 
-                // Stash away the last request details, if DEBUG active.
-                if (_this.DEBUG) {
+                // Stash away the last request details, if debug active.
+                if (_this.debug) {
                     window._lastreq = req;
                     window._lastobj = obj;
                 }
                 
                 // Dispatch to appropriate handler callback
-                if ( (req.status >= 400 || (obj && obj.Error) ) && kwArgs.error)
+                if ( (req.status >= 400 || (obj && obj.Error) ) && kwArgs.error) {
                     return kwArgs.error(req, obj);
-                else
+                } else {
                     return kwArgs.load(req, obj);
+                }
 
             }
-        }
+        };
         req.send(kwArgs.content);
         return req;
     },
 
-    /**
-        Turn a simple structure of nested XML elements into a 
-        JavaScript object.
-
-        TODO: Handle attributes?
-    */
-    xmlToObj: function(parent, force_lists, path) {
+    // Turn a simple structure of nested XML elements into a JavaScript object.
+    //
+    // TODO: Handle attributes?
+    xmlToObj: function (parent, force_lists, path) {
         var obj = {};
         var cdata = '';
         var is_struct = false;
 
         for(var i=0,node; node=parent.childNodes[i]; i++) {
-            if (3 == node.nodeType) { 
+            if (3 === node.nodeType) { 
                 cdata += node.nodeValue;
             } else {
                 is_struct = true;
@@ -305,7 +287,7 @@ S3Ajax = {
                     var do_force_list = false;
                     if (force_lists) {
                         for (var j=0,item; item=force_lists[j]; j++) {
-                            if (item == cpath) {
+                            if (item === cpath) {
                                 do_force_list=true; break;
                             }
                         }
@@ -325,23 +307,17 @@ S3Ajax = {
         return (is_struct) ? obj : cdata;
     },
 
-    /**
-        Abstract HMAC SHA1 signature calculation.
-    */
-    hmacSHA1: function(data, secret) {
-        // TODO: Alternate Dojo implementation?
+    // Abstract HMAC SHA1 signature calculation.
+    // see: http://pajhome.org.uk/crypt/md5/sha1.js
+    hmacSHA1: function (data, secret) {
         return b64_hmac_sha1(secret, data)+'=';
     },
     
-    /**
-        Return a date formatted appropriately for HTTP Date header.
-        Inspired by: http://www.svendtofte.com/code/date_format/
-
-        TODO: Should some/all of this go into common.js?
-    */
-    httpDate: function(d) {
+    // Return a date formatted appropriately for HTTP Date header.
+    // Inspired by: http://www.svendtofte.com/code/date_format/
+    httpDate: function (d) {
         // Use now as default date/time.
-        if (!d) d = new Date();
+        if (!d) { d = new Date(); }
 
         // Date abbreviations.
         var daysShort   = ["Sun", "Mon", "Tue", "Wed",
@@ -350,60 +326,55 @@ S3Ajax = {
                            "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
         // See: http://www.quirksmode.org/js/introdate.html#sol
-        function takeYear(theDate) {
+        function takeYear (theDate) {
             var x = theDate.getYear();
             var y = x % 100;
             y += (y < 38) ? 2000 : 1900;
             return y;
-        };
+        }
 
         // Number padding function
-        function zeropad(num, sz) { 
+        function zeropad (num, sz) { 
             return ( (sz - (""+num).length) > 0 ) ? 
                 arguments.callee("0"+num, sz) : num; 
-        };
+        }
         
-        function gmtTZ(d) {
+        function gmtTZ (d) {
             // Difference to Greenwich time (GMT) in hours
             var os = Math.abs(d.getTimezoneOffset());
             var h = ""+Math.floor(os/60);
             var m = ""+(os%60);
-            h.length == 1? h = "0"+h:1;
-            m.length == 1? m = "0"+m:1;
+            if (h.length === 1) { h = "0"+h; }
+            if (m.length === 1) { m = "0"+m; }
             return d.getTimezoneOffset() < 0 ? "+"+h+m : "-"+h+m;
-        };
+        }
 
-        var s;
-        s  = daysShort[d.getDay()] + ", ";
-        s += d.getDate() + " ";
-        s += monthsShort[d.getMonth()] + " ";
-        s += takeYear(d) + " ";
-        s += zeropad(d.getHours(), 2) + ":";
-        s += zeropad(d.getMinutes(), 2) + ":";
-        s += zeropad(d.getSeconds(), 2) + " ";
-        s += gmtTZ(d);
-
-        return s;
+        return [
+            daysShort[d.getDay()], ", ",
+            d.getDate(), " ",
+            monthsShort[d.getMonth()], " ",
+            takeYear(d), " ",
+            zeropad(d.getHours(), 2), ":",
+            zeropad(d.getMinutes(), 2), ":",
+            zeropad(d.getSeconds(), 2), " ",
+            gmtTZ(d)
+        ].join('');
     },
 
-    /* Help protect against errant end-commas */
-    EOF: null
-
-};
-
-if (!window['queryString']) {
-    // Swiped from MochiKit
-    function queryString(params) {
-        var l = [];
-        for (k in params) 
-            l.push(k+'='+encodeURIComponent(params[k]))
+    // Encode an object's properties as an query params
+    queryString: function (params) {
+        var k, l = [];
+        for (k in params) {
+            if (params.hasOwnProperty(k)) {
+                l.push(k+'='+encodeURIComponent(params[k]));
+            }
+        }
         return l.join("&");
-    }
-}
+    },
 
-if (!window['getXMLHttpRequest']) {
-    // Shamelessly swiped from MochiKit/Async.js
-    function getXMLHttpRequest() {
+    // Get an XHR object, somehow.
+    getXMLHttpRequest: function () {
+        // Shamelessly swiped from MochiKit/Async.js
         var self = arguments.callee;
         if (!self.XMLHttpRequest) {
             var tryThese = [
@@ -425,5 +396,6 @@ if (!window['getXMLHttpRequest']) {
         }
         return self.XMLHttpRequest();
     }
-}
+};
 
+/*jshint forin:true, noempty:true, eqeqeq:true, boss:true, bitwise:true, curly:true, browser:true, indent:4, maxerr:50 */
